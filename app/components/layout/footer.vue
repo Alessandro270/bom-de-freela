6c8454146636d5684e9caa825a0cd3aac8207b5c
Alessandro Almeida <propriedade0@gmail.com>
2026-03-19 13:26:49 +0100

    Fixing footer

--- app/components/layout/footer.vue
+++ app/components/layout/footer.vue
@@ -1,44 +1,54 @@
 <template>
-  <footer
-    class="py-16 flex px-20 space-x-20 min-h-80 bg-slate-950 text-slate-50"
-  >
-    <div>
-      <UiLogo class="mb-2" />
-      <p class="text-slate-500 max-w-80 text-justify font-medium text-sm">
-        A plataforma de freelancing que conecta talento e oportunidade em angola
-      </p>
-    </div>
-    <div>
-      <h1 class="text-lg font-semibold mb-2">Freelancers</h1>
-      <ul class="text-slate-500 space-y-1.5 font-medium text-sm">
-        <li>Encontrar freelancers</li>
-        <li>Criar Perfil</li>
-        <li>Ver freelancers</li>
-      </ul>
-    </div>
-    <div>
-      <h1 class="text-lg font-semibold mb-2">Clientes</h1>
-      <ul class="text-slate-500 space-y-1.5 font-medium text-sm">
-        <li>Publicar Projeto</li>
-        <li>Contratar freelancers</li>
-        <li>Começar agora</li>
-      </ul>
-    </div>
-    <div>
-      <h1 class="text-lg font-semibold mb-2">Empresa</h1>
-      <ul class="text-slate-500 space-y-1.5 font-medium text-sm">
-        <li>Sobre nós</li>
-        <li>Missão, visão e valores</li>
-      </ul>
-    </div>
+  <footer class="min-h-80 bg-slate-950 text-slate-50">
+    <div class="flex py-16 px-20 space-x-20">
+      <div>
+        <UiLogo class="mb-2" />
+        <p class="text-slate-500 max-w-80 text-justify font-medium text-sm">
+          A plataforma de freelancing que conecta talento e oportunidade em
+          angola
+        </p>
+      </div>
+      <div>
+        <h1 class="text-lg font-semibold mb-2">Freelancers</h1>
+        <ul class="text-slate-500 space-y-1.5 font-medium text-sm">
+          <li>Encontrar freelancers</li>
+          <li>Criar Perfil</li>
+          <li>Ver freelancers</li>
+        </ul>
+      </div>
+      <div>
+        <h1 class="text-lg font-semibold mb-2">Clientes</h1>
+        <ul class="text-slate-500 space-y-1.5 font-medium text-sm">
+          <li>Publicar Projeto</li>
+          <li>Contratar freelancers</li>
+          <li>Começar agora</li>
+        </ul>
+      </div>
+      <div>
+        <h1 class="text-lg font-semibold mb-2">Empresa</h1>
+        <ul class="text-slate-500 space-y-1.5 font-medium text-sm">
+          <li>Sobre nós</li>
+          <li>Missão, visão e valores</li>
+        </ul>
+      </div>
 
-    <div>
-      <h1 class="text-lg font-semibold mb-2">Suporte</h1>
-      <ul class="text-slate-500 space-y-1.5 font-medium text-sm">
-        <li>Telefone: +244 941 696 934</li>
-        <li>Email: propriedade0@gmail.com</li>
-        <li>Fazer pedido de suporte</li>
-      </ul>
+      <div>
+        <h1 class="text-lg font-semibold mb-2">Suporte</h1>
+        <ul class="text-slate-500 space-y-1.5 font-medium text-sm">
+          <li>Telefone: +244 941 696 934</li>
+          <li>Email: propriedade0@gmail.com</li>
+          <li>Fazer pedido de suporte</li>
+        </ul>
+      </div>
     </div>
+    <ul
+      class="flex space-x-4 items-center justify-center h-20 border-t border-t-slate-600 text-sm"
+    >
+      <li>
+        Copyright &copy; - 2026 | Bom de freela, todos os direitos reservados.
+      </li>
+      <li>Privacidade</li>
+      <li>Termos de uso</li>
+    </ul>
   </footer>
 </template>

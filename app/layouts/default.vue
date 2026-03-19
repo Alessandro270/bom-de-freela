e2a9e2e24fb5bd43304f37d6bbee620e1073e597
Alessandro Almeida <propriedade0@gmail.com>
2026-03-19 13:30:26 +0100

    Fixing layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <LayoutHeader />
-    <main class="min-h-screen">
+    <main class="min-h-screen px-20 py-14 bg-slate-900 h-screen text-slate-50">
       <slot />
     </main>
     <LayoutFooter />

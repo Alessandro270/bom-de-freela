0679e2cbcf60f239d3c5cc98e69ba649a9b7c9c6
Alessandro Almeida <propriedade0@gmail.com>
2026-03-20 18:12:22 +0100

    Adding app layout

--- app/components/app/layout/header.vue
+++ app/components/app/layout/header.vue
@@ -1,3 +1,24 @@
 <template>
-  <header>THIS IS A HEADER</header>
+  <header class="bg-slate-950 px-20 py-6 flex">
+    <UiLogo />
+    <div class="ml-auto">
+      <input
+        type="search"
+        placeholder="pesquisar..."
+        name="search"
+        id="search"
+      />
+    </div>
+    <nav>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+    <ul class="flex gap-6">
+      <li>notificacoes</li>
+      <li>conta</li>
+    </ul>
+  </header>
 </template>

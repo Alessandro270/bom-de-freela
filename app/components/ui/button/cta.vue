b08db694bf9969fba1d064da910846040cf8fd52
Alessandro Almeida <propriedade0@gmail.com>
2026-03-20 15:50:52 +0100

    Changing cta btn

--- app/components/ui/button/cta.vue
+++ app/components/ui/button/cta.vue
@@ -4,7 +4,7 @@ const btnType = !props.type ? "cta" : props.type;
 const colors =
   btnType === "cta"
     ? "hover:border-slate-50 bg-purple-600 hover:bg-transparent border-purple-600"
-    : "hover:border-transparent bg-transparent hover:bg-slate-950 border-slate-50";
+    : "hover:border-transparent bg-transparent hover:bg-slate-950 border-transparent";
 </script>
 
 <template>

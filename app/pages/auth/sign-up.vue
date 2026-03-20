d2fe714720d11dfc175cfc193f638221d6d20597
Alessandro Almeida <propriedade0@gmail.com>
2026-03-20 10:26:58 +0100

    Finishing signup-page

--- app/pages/auth/sign-up.vue
+++ app/pages/auth/sign-up.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="bg-slate-950 w-2/3 h-190 rounded-2xl overflow-hidden flex">
-    <div class="w-1/2 bg-slate-400 rounded-2xl"></div>
+  <div class="bg-slate-950 w-2/3 h-190 rounded-2xl flex">
+    <div class="w-1/2 bg-slate-400 rounded-2xl overflow-hidden">
+      <img
+        src="~/assets/img/signup-mobile.jpg"
+        srcset="~/assets/img/signup-desktop.jpg 2x"
+        alt="abstract-image"
+        class="h-full w-full object-cover"
+      />
+    </div>
     <form class="h-full flex flex-col w-1/2 items-center justify-center px-10">
       <div class="mb-10 text-center">
         <h1 class="text-2xl font-semibold mb-2">Cadastre-se</h1>
@@ -20,15 +27,19 @@
             title="Último nome"
           />
         </div>
-        <UiInputGroup name="email" placeholder="Olá mundo" title="Email" />
+        <UiInputGroup
+          name="email"
+          placeholder="exemplo@exemplo.com"
+          title="Email"
+        />
         <UiInputGroup
           name="password"
-          placeholder="example@example.com"
+          placeholder="Sua senha"
           title="Palavra passe"
         />
         <UiInputGroup
           name="confirmPassword"
-          placeholder="Olá mundo"
+          placeholder="Confirmar a senha"
           title="Confirmar palavra passe"
           class="mb-4"
         />
